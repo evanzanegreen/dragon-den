cd28f2270450b6f07a1b30010f369d184c20fe9a
Evan-Zane Green <evanzanegreen@hotmail.com>
2025-12-08 20:46:24 -0500

    Add Hero component

---
+++ src/components/Hero.jsx
@@ -0,0 +1,37 @@
+import "./Hero.css";
+
+function Hero() {
+  return (
+    <section className="hero">
+      {/*Background Video*/}
+      <video
+        className="hero-video"
+        src="/video/home-hero-2.mp4"
+        autoPlay
+        loop
+        muted
+        playsInline
+      />
+
+      {/*Hero Overlay*/}
+      <div className="hero-overlay" />
+
+      {/*Content*/}
+      <div className="hero-content">
+        <h2 className="hero-sm-title">WELCOME TO THE</h2>
+        <h1 className="hero-title">DEN.</h1>
+        <h3 className="subtitle">
+          Authentic Caribbean Flavors. Bold Spices & an Unforgettable
+          Atmosphere.
+        </h3>
+
+        <div className="buttons">
+          <button className="menu">View Menu</button>
+          <button className="reservation">Make Reservation</button>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
